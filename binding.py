fe5830a161b1912b6dd19fb21044ebb799a8d0c1
aax5 <aaz24@pitt.edu>
2017-01-27 00:11:04 -0500

    Update binding.py

--- binding.py
+++ binding.py
@@ -12,34 +12,36 @@ txtLength = 0 #how long the file should be
 #for txtLength in xrange(len(PdbData))
 for line in f:
 #line is now string of single line in f
-  words = line.split()
-  print words
-  proteinName = words[0]
-  restOfData = words[3]
-  #finds the proteinName and the part of data with binding affinities
+  if line[0] != "#"
+    
+    words = line.split()
+    print words
+    proteinName = words[0]
+    restOfData = words[3]
+    #finds the proteinName and the part of data with binding affinities
 
-  match = re.search(r'(\S+)=(\d+.?\d*)(\SM)', PdbData)
-  """matches types: group(1) matches the nonspace chars before the "=", group(2) matches the numerical chars
-  and ".", and group(3) matches the nonspace chars after those numbers  """
+    match = re.search(r'(\S+)=(\d+.?\d*)(\SM)', PdbData)
+    """matches types: group(1) matches the nonspace chars before the "=", group(2) matches the numerical chars
+    and ".", and group(3) matches the nonspace chars after those numbers  """
   
-  source = match.group(1)
-  #gives prefix before data
-  baseNum = match.group(2)
-  #gives baseNum before unit conversion
-  measurementValue = match.group(3)
-  #gives measurement unit
+    source = match.group(1)
+    #gives prefix before data
+    baseNum = match.group(2)
+    #gives baseNum before unit conversion
+    measurementValue = match.group(3)
+    #gives measurement unit
   
-  if (measurementValue == "mM"):
+    if (measurementValue == "mM"):
         endNum = float(baseNum)*0.001    
-  elif (measurementValue == "uM"):
+    elif (measurementValue == "uM"):
         endNum = float(baseNum)*0.000001  
-  elif (measurementValue == "nM"):
+    elif (measurementValue == "nM"):
         endNum = float(baseNum)*0.000000001   
-  elif (measurementValue == "pM"):
+    elif (measurementValue == "pM"):
         endNum = float(baseNum)*0.000000000001
-  else:
+    else:
         endNum = -1
-  endString += source + " " + baseNum + " " + measurementValue + "\n"
+    endString += source + " " + baseNum + " " + measurementValue + "\n"
     # converts from whatever measurement type given to M and saves it as an int, endNum
 out = open('/home/aaz24/output.txt','w')
 out.write(endString)
