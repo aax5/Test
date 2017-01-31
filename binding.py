d0f0be4884af76970737fd5a73972ef1fbdbe21b
aax5 <aaz24@pitt.edu>
2017-01-31 17:55:41 -0500

    Update binding.py

--- binding.py
+++ binding.py
@@ -8,7 +8,7 @@ parser.add_argument("-i", "--input", help = "input file")
 parser.add_argument("-o", "--output", help = "output file")
 args = parser.parse_args()
 f = open(args.input)
-outputFile = open(args.output)
+outputFile = open(args.output, 'w')
 #print f
 #f = open('/home/dkoes/PDBbind/index/INDEX_general_PL.2016')
 #test string
