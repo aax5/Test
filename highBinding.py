1a7bc3ef51822e6005f07ec05280224a6122c9be
aax5 <aaz24@pitt.edu>
2017-02-02 19:52:40 -0500

    Create highBinding.py

---
+++ highBinding.py
@@ -0,0 +1,11 @@
+import argparse
+#PdbData = "2w0x  2.12  2009  IC50=625uM    // 2w0x.pdf (PD2) compound 4" #in here should go the line that is fed in from the data
+parser = argparse.ArgumentParser(description = "Files to be used")
+parser.add_argument("-i", "--input", help = "input file")
+parser.add_argument("-o", "--output", help = "output file")
+args = parser.parse_args()
+f = open(args.input)
+outputFile = open(args.output, 'w')
+for line in f:
+  words=line.split()
+  print words
