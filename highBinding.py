58c5a9c8ac0dfcf50a37f339e582e588043b9207
aax5 <aaz24@pitt.edu>
2017-02-12 22:14:14 -0500

    Update highBinding.py

--- highBinding.py
+++ highBinding.py
@@ -17,8 +17,8 @@ for line in f:
     m = re.search(r'(\w+)_rec', vals[1])
     if m:
         pdb = m.group(1)
-        
-        if affs[pdb] > 2 and pdb in affs:
+        float(a) = affs[pdb]
+        if a > 2 and pdb in affs:
           print line.rstrip()
     else:
         sys.stderr.write("Problem with: %s\n" % line)
