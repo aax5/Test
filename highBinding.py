a68e1dfae1cd6cdce39fe66f57b91ad7419c83f8
aax5 <aaz24@pitt.edu>
2017-02-15 18:54:50 -0500

    Update highBinding.py

--- highBinding.py
+++ highBinding.py
@@ -1,27 +1,27 @@
 import sys, re
 
 if len(sys.argv) != 3:
-    print "Need train file and affinities dictionary"
-    sys.exit(1)
+  print "Need train file and affinities dictionary"
+  sys.exit(1)
 
 f = open(sys.argv[1])
 
 affs = dict()
 for line in open(sys.argv[2]):
   #get dictionary, sets all proteins in second file in dict
-    (pdb,_,a) = line.split()
-    affs[pdb] = float(a)
+  (pdb,_,a) = line.split()
+  affs[pdb] = float(a)
 
 for line in f:
-    vals = line.split()
-    m = re.search(r'(\w+)_rec', vals[1])
-    if m:
-        #print pdb + _ + str(a)
-        pdb = m.group(1)
-        if pdb in affs:
-            a = affs[pdb]
-            if a < 3:
+  vals = line.split()
+  m = re.search(r'(\w+)_rec', vals[1])
+  if m:
+      #print pdb + _ + str(a)
+      pdb = m.group(1)
+      if pdb in affs:
+      a = affs[pdb]
+      if a < 3:
                 #print line.rstrip()
 
-    else:
-        sys.stderr.write("Problem with: %s\n" % line)
+  else:
+      sys.stderr.write("Problem with: %s\n" % line)
