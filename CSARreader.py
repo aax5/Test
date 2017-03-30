b2772d26046a462677df90157a2eba5ac8b1dbbe
aax5 <aaz24@pitt.edu>
2017-03-30 00:43:56 -0400

    Update CSARreader.py

--- CSARreader.py
+++ CSARreader.py
@@ -7,10 +7,17 @@ for line in open(sys.argv[1]):
   if match:
     print match.group()
     name = match.group()
-    proteins[name]++
+    proteins[name] = 0
+    
     
   else:
     print 'error'
+    
+for line in open(sys.argv[1]):
+  vals = line.split()
+  match = re.search(r'/\w+/', vals[1])
+  if match:
+    proteins[name]++
 print proteins 
   
   
