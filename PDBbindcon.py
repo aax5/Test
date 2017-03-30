a0c998dc9a1bc8d8cb1a481bf6c67b3b02e63832
aax5 <aaz24@pitt.edu>
2017-03-30 15:43:57 -0400

    Update PDBbindcon.py

--- PDBbindcon.py
+++ PDBbindcon.py
@@ -26,6 +26,8 @@ x = (sys.argv[2])
 for i in range (0,int(x)): 
   name, val = random.choice(list(proteins.items()))
   randProteins[name] = val
+  
+  
 for line in open(sys.argv[1]):
   vals = line.split
   match = re.search(r'\w+/', vals[1])
