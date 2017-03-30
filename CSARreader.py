bc0fb549ae6e23334748b2ae565b298768a5efa4
aax5 <aaz24@pitt.edu>
2017-03-30 00:13:09 -0400

    Update CSARreader.py

--- CSARreader.py
+++ CSARreader.py
@@ -1,9 +1,13 @@
 import sys, re
-affs = dict
+proteins = dict
 for line in open(sys.argv[1]):
   vals = line.split()
   match = re.search(r'/\w+/', vals[1])
   if match:
     print match.group()
+    proteins[name] = match.group
   else:
     print 'error'
+print proteins 
+  
+  
