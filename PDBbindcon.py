a28db33eb54c9457cda20340505bfc5fb45c6c41
aax5 <aaz24@pitt.edu>
2017-03-30 15:17:47 -0400

    Create PDBbindcon.py

---
+++ PDBbindcon.py
@@ -0,0 +1,21 @@
+import system, re
+
+for line in open(sys.argv[1]):
+  vals = line.split()
+  match = re.search(r'/w+/', vals[1])
+  if match:
+    print match.group()
+    name = match.group()
+    proteins[name] = 0
+    
+  else:
+    print 'error'
+    
+for line in open(sys.argv[1]):
+  vals = line.split()
+  match = re.search(r'/\w+/', vals[1])
+  if match:
+    if match.group() in proteins:
+      proteins[match.group()] += 1
+      
+print proteins 
