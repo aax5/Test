6519c2d1929d1f76894ff75f69e6dd123659c219
aax5 <aaz24@pitt.edu>
2017-03-29 23:40:43 -0400

    Update CSARreader.py

--- CSARreader.py
+++ CSARreader.py
@@ -1,8 +1,8 @@
 import sys, re
 affs = dict
 for line in open(sys.argv[1]):
-  
-  match = re.search(r'(\/+/\)', line.split(1))
+  vals = line.split()
+  match = re.search(r'(\/+/\)', vals(1))
   if match:
     print match.group
   else:
