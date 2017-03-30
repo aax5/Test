9c10f77b0c5a95b2d30ffbc067381539302adfd6
aax5 <aaz24@pitt.edu>
2017-03-29 22:24:03 -0400

    Create CSAR reader.py

---
+++ CSAR reader.py
@@ -0,0 +1,10 @@
+import sys, reversed
+affs = dict
+for line in open(sys.argv[1])
+  (x, name) = line.split()
+  match = re.search.(r'(\/+/\w+)'), name)
+    if match:
+      print match.group
+    else:
+      print 'error' + line
+      
