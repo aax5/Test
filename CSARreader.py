bb2a9739f2de438e23a6f36e01c527f0f678555c
aax5 <aaz24@pitt.edu>
2017-03-29 23:56:33 -0400

    Update CSARreader.py

--- CSARreader.py
+++ CSARreader.py
@@ -1,10 +1,1 @@
-import sys, re
-affs = dict
-for line in open(sys.argv[1]):
-  vals = line.split()
-  match = re.search(r'/\w+/', vals[1])
-  if match:
-    print match.group
-  else:
-    print 'error' + line
-      
+
