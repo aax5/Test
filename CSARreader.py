e911108c10ab3d32e63ef7296c660e6fc8efd10b
aax5 <aaz24@pitt.edu>
2017-03-29 23:57:27 -0400

    Update CSARreader.py

--- CSARreader.py
+++ CSARreader.py
@@ -1,1 +1,10 @@
 
+affs = dict
+ for line in open(sys.argv[1]):
+   vals = line.split()
+-  match = re.search(r'/+/', vals[1])
++  match = re.search(r'/\w+/', vals[1])
+   if match:
+     print match.group()
+   else:
+     print 'error'
