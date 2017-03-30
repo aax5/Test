e3aef145704280277cb8a9a7bc41092f853a954d
aax5 <aaz24@pitt.edu>
2017-03-30 15:45:13 -0400

    Update PDBbindcon.py

--- PDBbindcon.py
+++ PDBbindcon.py
@@ -29,7 +29,7 @@ for i in range (0,int(x)):
   
   
 for line in open(sys.argv[1]):
-  vals = line.split
+  vals = line.split()
   match = re.search(r'\w+/', vals[1])
   if match:
     print match.group()
