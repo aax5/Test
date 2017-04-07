2c93fedca1fd015c18f5a228067368bba36681d7
aax5 <aaz24@pitt.edu>
2017-04-06 23:34:09 -0400

    Update makeTrain.py

--- makeTrain.py
+++ makeTrain.py
@@ -4,8 +4,8 @@ import os, sys
 x = sys.argv[1]
 for counter in range(0,int(x)):
   files = int(counter)*1000
-  dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain1.types"
-  dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain1.types"
+  dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain2.types"
+  dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain2.types"
   
   cmd1 = ("shuf -n " + str(files) + " "  + dir1 + " > " + dir2)
   
