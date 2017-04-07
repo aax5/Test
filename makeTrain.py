d50528ffb2ca2d8d72396d99b99a1ff450f46dff
aax5 <aaz24@pitt.edu>
2017-04-06 23:25:54 -0400

    Update makeTrain.py

--- makeTrain.py
+++ makeTrain.py
@@ -1,6 +1,12 @@
 import os, sys
 
-x = sys.argv[1]
-shuf -n int(x) pdbbindtrain0.types > x + newpdbtrain0.types= ("cp -r " + /home/aaz24 + dir2)
 
-    os.system(cmd1)
+x = sys.argv[1]
+for x in range(0,40):
+  files = int(x)*1000
+  dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain0.types"
+  dir2 = /home/dkoes/PDBbind/refined-set/new + x + pdbtrain0.types
+  
+  cmd1 = ("shuf -n " + files + " "  + dir1 + " > " + dir2)
+  
+  os.system(cmd1)
