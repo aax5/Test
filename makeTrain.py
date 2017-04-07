85faf6325fae12cc995f609b6755c8773c7c8304
aax5 <aaz24@pitt.edu>
2017-04-06 23:46:57 -0400

    Update makeTrain.py

--- makeTrain.py
+++ makeTrain.py
@@ -3,11 +3,6 @@ import os, sys
 
 x = sys.argv[1]
 """for counter in range(0,int(x)):
-  files = int(counter)*1000
-  dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain2.types"
-  dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain2.types"
-  
-  cmd1 = ("shuf -n " + str(files) + " "  + dir1 + " > " + dir2)
   
   os.system(cmd1)"""
 for counter in range(0,int(x)):
@@ -15,6 +10,12 @@ for counter in range(0,int(x)):
   for newcounter in range(0,3):
     
     dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtest" + str(newcounter) + ".types"
-    dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain" + str(newcounter-1)+ ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtest" + str(newcounter)+ ".types"
     cmd2 = "more " + dir1 + " > " + dir2
     os.system(cmd2)
+    
+    dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain" + str(newcounter) + ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain" + str(newcounter) + ".types"
+
+    cmd1 = ("shuf -n " + str(files) + " "  + dir1 + " > " + dir2)
+
