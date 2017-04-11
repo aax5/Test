0c5435d8bae04484d423b40b0207b1e903d28877
aax5 <aaz24@pitt.edu>
2017-04-11 19:31:16 -0400

    Update makeTrain.py

--- makeTrain.py
+++ makeTrain.py
@@ -11,12 +11,12 @@ for counter in range(0,int(x)):
   for newcounter in range(0,3):
     
     dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtest" + str(newcounter) + ".types"
-    dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtest" + str(newcounter)+ ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/new/new" + str(files) + "pdbtest" + str(newcounter)+ ".types"
     cmd2 = "more " + dir1 + " > " + dir2
     os.system(cmd2)
     
     dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain" + str(newcounter) + ".types"
-    dir2 = "/home/dkoes/PDBbind/refined-set/new" + str(files) + "pdbtrain" + str(newcounter) + ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/new/new" + str(files) + "pdbtrain" + str(newcounter) + ".types"
 
     cmd1 = ("shuf -n " + str(files) + " "  + dir1 + " > " + dir2)
     os.system(cmd1)
