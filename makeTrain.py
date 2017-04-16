d831df5c1e883ca03a520ca0b6df67c842949f3a
aax5 <aaz24@pitt.edu>
2017-04-16 02:57:15 -0400

    Update makeTrain.py

--- makeTrain.py
+++ makeTrain.py
@@ -10,13 +10,13 @@ for counter in range(0,int(x)):
   files = int(counter)*int(multiplier)
   for newcounter in range(0,3):
     
-    dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtest" + str(newcounter) + ".types"
-    dir2 = "/home/dkoes/PDBbind/refined-set/new/new" + str(files) + "pdbtest" + str(newcounter)+ ".types"
+    dir1 = "/home/dkoes/PDBbind/refined-set/nopospdbbindtest" + str(newcounter) + ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/nopos/new" + str(files) + "nopospdbtest" + str(newcounter)+ ".types"
     cmd2 = "more " + dir1 + " > " + dir2
     os.system(cmd2)
     
-    dir1 = "/home/dkoes/PDBbind/refined-set/pdbbindtrain" + str(newcounter) + ".types"
-    dir2 = "/home/dkoes/PDBbind/refined-set/new/new" + str(files) + "pdbtrain" + str(newcounter) + ".types"
+    dir1 = "/home/dkoes/PDBbind/refined-set/nopospdbbindtrain" + str(newcounter) + ".types"
+    dir2 = "/home/dkoes/PDBbind/refined-set/nopos/new" + str(files) + "nopospdbtrain" + str(newcounter) + ".types"
 
     cmd1 = ("shuf -n " + str(files) + " "  + dir1 + " > " + dir2)
     os.system(cmd1)
